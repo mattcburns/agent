e0b042538a379fe7193cf255b7d1578fc31b7342
Matthew Burns <me@mattcburns.com>
2020-08-10 23:07:38 -0700

    Prints the serial now!
    Super ugly enum matching to get the serial number out - there must be a nicer way to do this but this works for now.

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,38 @@
 use rustbus::{RpcConn, MessageBuilder, client_conn::Timeout, params::Container};
+use rustbus::params::{Param, Base};
 
 fn main() -> Result<(), rustbus::client_conn::Error> {
-    // Connect to the session bus
     let mut rpc_con = RpcConn::system_conn(Timeout::Infinite)?;
 
-    // create a signal with the MessageBuilder API
     let mut sig = MessageBuilder::new()
-    .call("Get".into())
-    .on("/xyz/openbmc_project/inventory/system".into())
-    .with_interface("org.freedesktop.DBus.Properties".into())
-    .at("xyz.openbmc_project.Inventory.Manager".into())
-    .build();
+        .call("Get".into())
+        .on("/xyz/openbmc_project/inventory/system".into())
+        .with_interface("org.freedesktop.DBus.Properties".into())
+        .at("xyz.openbmc_project.Inventory.Manager".into())
+        .build();
 
-    sig.body.push_param2(String::from("xyz.openbmc_project.Inventory.Decorator.AssetTag").as_str(),
-        String::from("AssetTag").as_str()).expect("Unable to push_param2");
-    // send a signal to all bus members
+    // Additional params to retrieve the AssetTag properties.
+    sig.body.push_param2(
+        String::from("xyz.openbmc_project.Inventory.Decorator.AssetTag").as_str(),
+        String::from("AssetTag").as_str())
+        .expect("Unable to push_param2");
     let serial = rpc_con.send_message(&mut sig, Timeout::Infinite)?;
     let resp = rpc_con.wait_response(serial, Timeout::Infinite)?;
-    
-    let um = resp.unmarshall_all();
 
-    match um {
-        Container::Variant::Variant => println!(sig),
-        _ => println!("oops, fix this"),
+    let r = resp.unmarshall_all()?;
+
+    let s = &r.params[0];
+
+    // This is super ugly, need to figure out how to improve
+    if let Param::Container(v) = s {
+        if let Container::Variant(x) = v {
+            if let Param::Base(y) = &x.value {
+                if let Base::String(z) = &y {
+                    println!("{:?}", z)
+                }
+            }
+        }
     }
-
-    //println!("{:?}", resp.unmarshall_all()?);
-    
 
     Ok(())
 }
