1be6d5ef1ce1c8249f8be4b084de8048dd690625
Matthew Burns <me@mattcburns.com>
2020-08-10 23:26:30 -0700

    Actually remove vedor

--- vendor/rustbus/src/peer/mod.rs
+++
@@ -1,6 +0,0 @@
-//! This module implemets the org.freedesktop.DBus.Peer
-//!
-//! This might be useful for users of this library, but is kept optional
-
-mod peer_handling;
-pub use peer_handling::*;
